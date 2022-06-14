01ba8da0f9fcd74e89bd14edbb8bb0f3956fd0b7
lackatacker <lackatacker@DESKTOP-N3V1CK4>
2022-06-14 12:27:49 +0100

    first commit

---
+++ src/views/Products.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Breadc />
+    <Grid />
+  </div>
+</template>
+
+<script>
+import Breadc from '@/Components/ProductsPage/Breadc.vue'
+import Grid from '@/Components/ProductsPage/Grid.vue'
+
+export default {
+  components: {
+    Breadc, Grid
+  },
+  created() {
+    var itemsStr = localStorage.getItem('cartItems')
+    if (itemsStr) {
+      this.$store.state.cartItems = []
+      var items = JSON.parse(itemsStr)
+      for (var i = 0; i < items.length; i++) {
+        this.$store.commit('inCart', items[i])
+      }
+    }
+  }
+}
+</script>
